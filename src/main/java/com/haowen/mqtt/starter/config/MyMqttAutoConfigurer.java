9baa284e08f694c211fb715732ab323583654581
dmwhw <dmwhw@qq.com>
2019-11-21 15:22:31 +0800

    修改haowen.mqtt.starter.enable=true 的bug

--- src/main/java/com/haowen/mqtt/starter/config/MyMqttAutoConfigurer.java
+++ src/main/java/com/haowen/mqtt/starter/config/MyMqttAutoConfigurer.java
@@ -1,6 +1,7 @@
 package com.haowen.mqtt.starter.config;
 
 import java.util.List;
+
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,40 +22,39 @@ import com.haowen.mqtt.core.listener.MqttMsgArrivedListener;
 
 @Configuration
 @EnableConfigurationProperties(value = MyMqttConfigBean.class)
-@ConditionalOnClass({MqttClient.class,MyMQTTClient.class})
-@ConditionalOnProperty(prefix = "haowen.mqtt.starter", name="enable", havingValue="true")
+@ConditionalOnClass({MqttClient.class, MyMQTTClient.class})
 public class MyMqttAutoConfigurer {
-	private final static  Logger log= LoggerFactory.getLogger(MyMqttAutoConfigurer.class);
+    private final static Logger log = LoggerFactory.getLogger(MyMqttAutoConfigurer.class);
 
-	
-	public final static String myMQTTClient="myMQTTClient";
-	@Autowired
-	private MyMqttConfig mqttConfig;
+    public final static String myMQTTClient = "myMQTTClient";
 
-	
-	@Autowired(required=false)
-	private List<MqttListener> mqttListeners;
-	
-	@Autowired(required=false)
-	private List<MqttConnectlLostListener> mqttConnectlLostListeners;
-	
-	@Autowired(required=false)
-	private List<MqttDeliveryCompleteListener> mqttDeliveryCompleteListeners;
-	
-	@Autowired(required=false)
-	private List<MqttMsgArrivedListener> mqttMsgArrivedListeners;
-	
-	
-	@Bean
-	public MyMQTTClient  myMQTTClient() {
-		List< MqttListener> _mqttListeners=Filter.filterAndSort(mqttListeners, myMQTTClient);
-		List<MqttConnectlLostListener> _mqttConnectlLostListeners=Filter.filterAndSort(mqttConnectlLostListeners, myMQTTClient);
-		List<MqttDeliveryCompleteListener> _mqttDeliveryCompleteListeners=Filter.filterAndSort(mqttDeliveryCompleteListeners, myMQTTClient);
-		List<MqttMsgArrivedListener> _mqttMsgArrivedListeners=Filter.filterAndSort(mqttMsgArrivedListeners, myMQTTClient);
-		return MyMqttInitializer.init(mqttConfig, _mqttListeners ,  _mqttConnectlLostListeners ,  _mqttDeliveryCompleteListeners ,   _mqttMsgArrivedListeners );
-	}
-	
- 
-	
- 	
+    @Autowired(required = false)
+    private MyMqttConfig mqttConfig;
+
+    @Autowired(required = false)
+    private List<MqttListener> mqttListeners;
+
+    @Autowired(required = false)
+    private List<MqttConnectlLostListener> mqttConnectlLostListeners;
+
+    @Autowired(required = false)
+    private List<MqttDeliveryCompleteListener> mqttDeliveryCompleteListeners;
+
+    @Autowired(required = false)
+    private List<MqttMsgArrivedListener> mqttMsgArrivedListeners;
+
+    @Bean
+    @ConditionalOnProperty(prefix = "haowen.mqtt.starter", name = "enable", havingValue = "true")
+    public MyMQTTClient myMQTTClient() {
+        List<MqttListener> _mqttListeners = Filter.filterAndSort(mqttListeners, myMQTTClient);
+        List<MqttConnectlLostListener> _mqttConnectlLostListeners =
+            Filter.filterAndSort(mqttConnectlLostListeners, myMQTTClient);
+        List<MqttDeliveryCompleteListener> _mqttDeliveryCompleteListeners =
+            Filter.filterAndSort(mqttDeliveryCompleteListeners, myMQTTClient);
+        List<MqttMsgArrivedListener> _mqttMsgArrivedListeners =
+            Filter.filterAndSort(mqttMsgArrivedListeners, myMQTTClient);
+        return MyMqttInitializer.init(mqttConfig, _mqttListeners, _mqttConnectlLostListeners,
+            _mqttDeliveryCompleteListeners, _mqttMsgArrivedListeners);
+    }
+
 }
--- src/test/java/com/haowen/MultiUserTestComp.java
+++ src/test/java/com/haowen/MultiUserTestComp.java
@@ -2,7 +2,8 @@ package com.haowen;
 
 import java.util.List;
 
-import org.springframework.context.annotation.Bean;
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Component;
 
 import com.haowen.mqtt.core.MyMQTTClient;
@@ -13,20 +14,18 @@ import com.haowen.mqtt.core.listener.MqttListener;
 @Component
 public class MultiUserTestComp {
 
-	
+    @Resource()
+    MyMqttConfig prop;
 
-	
-	
-	@Bean
-	public MyMQTTClient nonSslMQTTClient(List<MqttListener> mqttListeners,MyMqttConfig prop){
-		MyMqttConfig clone = (MyMqttConfig) prop.clone();
-		clone.setMqttUser("admin_d");
-		clone.setMqttpw("123456a");
-		clone.setOnStartedSubcribeTopics(new String[]{"$local/233933df730941bda9399fc706183966/cmdReturn"} );
-		//直接赋值 listener
-		return MyMqttInitializer.init(clone, mqttListeners, null, null, null);
-		  
-	}
-	
-	
+    // @Bean
+    public MyMQTTClient nonSslMQTTClient(List<MqttListener> mqttListeners) {
+        MyMqttConfig clone = (MyMqttConfig)prop.clone();
+        clone.setMqttUser("admin_d444");
+        clone.setMqttpw("123456a");
+        clone.setOnStartedSubcribeTopics(new String[] {"$local/233933df730941bda9399fc706183966/cmdReturn"});
+        // 直接赋值 listener
+        return MyMqttInitializer.init(clone, mqttListeners, null, null, null);
+
+    }
+
 }
